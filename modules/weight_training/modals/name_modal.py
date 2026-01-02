61b81f0a09b53a4826723b5f38f835e7ca77ce7d
0xteddie <251090332+0xteddie@users.noreply.github.com>
2026-01-01 23:30:18 -0800

    Created a new modal for editing existing exercise data and added a new key to self.data with the index value of the selector

--- modules/weight_training/modals/name_modal.py
+++ modules/weight_training/modals/name_modal.py
@@ -23,6 +23,15 @@ class NamePlanModal(discord.ui.Modal, title="Name Your Workout Plan"):
 
 
 class PlanDetails(discord.ui.Modal, title="Exercise details"):
+    """
+    Handles exercise detail submission.
+
+    Collects user-entered exercise data (name, sets, reps), appends it to the
+    existing plan data structure, and triggers a UI re-render to display the
+    updated plan.
+    
+    """
+
     exercise_name = discord.ui.TextInput(
         label="Exercise Name: ",
         placeholder="e.g. Pull up, Push ups",
@@ -43,14 +52,44 @@ class PlanDetails(discord.ui.Modal, title="Exercise details"):
         self.details = {}
 
     async def on_submit(self, interaction: discord.Interaction):
+        # Fetch the data from the user input
         self.details["exercise_name"] = self.exercise_name.value
         self.details["sets_count"] = self.sets_count.value
         self.details["reps_count"] = self.reps_count.value
         
         # Add into list
         self.data['data'].append(self.details)
-        print(self.data)
-
+        self.data['index_selected_value'] = 0
+        
         # Send the info back to the embed with the new data
         from ui.render import render
         await render(interaction, "new_plan", self.data)
+
+class EditExerciseDetails(discord.ui.Modal, title="Edit Exercise details"):
+    exercise_name = discord.ui.TextInput(
+        label="Exercise Name: ",
+        placeholder="e.g. Pull up, Push ups",
+        max_length=25
+    )
+    sets_count = discord.ui.TextInput(
+        label="Total Sets: ",
+        max_length=3
+    )
+    reps_count = discord.ui.TextInput(
+        label="Total Reps: ",
+        max_length=3
+    )
+
+    def __init__(self, data: dict):
+        super().__init__()
+        self.data = data
+        
+        edit_index = data["index_selected_value"]
+        
+        exercise = data["data"][edit_index]
+    
+        # Pre-fill inputs with selector value
+        self.exercise_name.default = str(exercise.get("exercise_name", ""))
+        self.sets_count.default = str(exercise.get("sets_count", ""))
+        self.reps_count.default = str(exercise.get("reps_count", ""))
+    
