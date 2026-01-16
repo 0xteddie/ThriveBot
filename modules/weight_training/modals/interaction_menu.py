34e9a3ca9a351b6519deca7f414a5ad52f6fac3e
0xteddie <251090332+0xteddie@users.noreply.github.com>
2026-01-15 20:51:47 -0800

    feat: Added a new select specific to workout plans

--- modules/weight_training/modals/interaction_menu.py
+++ modules/weight_training/modals/interaction_menu.py
@@ -28,3 +28,24 @@ class ExerciseSelect(discord.ui.Select):
         # Parsing the interaction payload - sent by discord.
         selected_index = int(self.values[0])
         chosen = self.data["data"][selected_index]
+
+class WorkOutPlan(discord.ui.Select):
+    def __init__(self, row=1):
+        options = [
+            discord.SelectOption(
+                label="Select 1",
+                value="select_1"
+            ),
+            discord.SelectOption(
+                label="Select 2",
+                value="select_2"
+            )
+        ]
+
+        super().__init__(
+            placeholder="Choose…",
+            options=options,
+            min_values=1,
+            max_values=1,
+            row=row
+        )
