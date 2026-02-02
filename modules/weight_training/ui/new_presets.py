7ee14ff5749b3ae014cb4ca0f4ecf04ce591399a
0xteddie <251090332+0xteddie@users.noreply.github.com>
2026-02-01 18:51:56 -0800

    refactor: new_presets.py is a replacement for embed_presets.py

---
+++ modules/weight_training/ui/new_presets.py
@@ -0,0 +1,44 @@
+import discord
+import datetime
+
+# Buttons
+from views.home_workout_view import HomeView
+from views.start_workout_view import StartWorkOutView
+from views.active_session_workout_view import WorkoutSessionView
+from views.exercise_detail_workout_view import NewPlanView, EditExerciseView, ExitPlanView
+
+# Emebeds
+from ui.active_embed import start_workout_session
+from ui.home_workout_embed import home_embed, workout_plans_list_embed
+from ui.exercise_detail_embed import new_plan_embed
+
+# EMBEDS
+EMBEDS = {
+    "home": home_embed,
+    "start": workout_plans_list_embed,
+    "start_workout": start_workout_session,
+    
+
+    ################# EDITING SECTION ONLY
+    "new_plan": new_plan_embed,
+    
+    "edit_new_plan": new_plan_embed, 
+    "exit_plan": new_plan_embed,
+
+    # "adjust": workout_plans_list_embed,
+}
+
+# BUTTONS.
+BUTTONS = {
+    "home": HomeView,
+    "start": StartWorkOutView,
+    "start_workout": WorkoutSessionView,
+    
+    ################# Editing only
+    "new_plan": NewPlanView,
+
+    "edit_new_plan": EditExerciseView,
+    
+    "exit_plan": ExitPlanView,
+    # "adjust": AdjustView,
+}
